a3b990385f3cec523ed7e03a525dc7d637f6e23d
github username <github email address>
2019-02-26 13:31:59 -0500

    Done.

--- reducer.js
+++ reducer.js
@@ -1,32 +1,3 @@
-let state;
-
-function changeCount(state = { count: 0 }, action) {
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return { count: state.count + 1 };
-
-    default:
-      return state;
-  }
-};
-
-function dispatch(action){
-  state = changeCount(state, action);
-  render();
-};
-
-function render() {
-  let container = document.getElementById('container');
-  container.textContent = store.getState().count;
-};
-
-dispatch({ type: '@@INIT' })
-let button = document.getElementById('button');
-
-button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
-})
-
 function createStore() {
   let state;
  
@@ -44,3 +15,26 @@ function createStore() {
     getState
   };
 };
+ 
+function changeCount(state = { count: 0 }, action) {
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return { count: state.count + 1 };
+ 
+    default:
+      return state;
+  }
+};
+ 
+function render() {
+  let container = document.getElementById('container');
+  container.textContent = store.getState().count;
+};
+ 
+let store = createStore();
+store.dispatch({ type: '@@INIT' });
+let button = document.getElementById('button');
+ 
+button.addEventListener('click', function() {
+    store.dispatch({ type: 'INCREASE_COUNT' });
+})
